5ee98281906ac7942aea32be76e8ada27658098a
Sebastian fliszkiewicz <sflisziewicz@live.market>
2022-02-04 23:45:35 +0100

    Init project and setup flavors

---
+++ lib/config/garden_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GardenApp extends StatelessWidget {
+  const GardenApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
---
+++ lib/main_prod.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:garden_app/config/garden_app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const GardenApp());
+}
